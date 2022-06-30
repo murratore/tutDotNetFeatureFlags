35486e2729bf330fc42f09ec84819a246b48ba50
murratore <mail@michael-maurer.ch>
2022-06-30 06:52:06 +0000

    Umstellung auf Microsoft.FeatureManagement

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.FeatureManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddFeatureManagement(builder.Configuration.GetSection("FeatureFlags"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -21,11 +26,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (IFeatureManager manager) =>
 {
-    if (!builder.Configuration.GetValue<bool>("FeatureFlags:WeatherForecast"))
-    {
-        return Results.NotFound();
+    if (!await manager.IsEnabledAsync("WeatherForecast"))   
+    {        
+        return Results.Content("not found");
     }
 
     var forecast =  Enumerable.Range(1, 5).Select(index =>
